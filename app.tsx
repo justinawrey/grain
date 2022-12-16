2a1befce4c3a558284c4fbea76739f4d66c9dcfd
Justin Awrey <awreyjustin@gmail.com>
2022-12-16 12:33:43 -0800

    simple watch

--- app.tsx
+++ app.tsx
@@ -1,4 +1,4 @@
-import { createSignal, mount } from "./lib/mod.ts";
+import { mount, reactive, watch } from "./lib/mod.ts";
 
 interface TextProps {
   name: string;
@@ -9,7 +9,12 @@ function Text({ name }: TextProps) {
 }
 
 function App() {
-  const [count, setCount] = createSignal(0);
+  const [count, setCount] = reactive(0);
+
+  watch(
+    count,
+    (prev, curr) => console.log("Count changed from", prev, "to", curr),
+  );
 
   return (
     <div>
